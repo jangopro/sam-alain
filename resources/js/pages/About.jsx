3c67702b90f97e5bb1562df04ba5f1c075a2a60a
Michael Provencher <michaelprovencher15@gmail.com>
2022-04-11 23:38:16 -0400

    use real text

--- resources/js/pages/About.jsx
+++ resources/js/pages/About.jsx
@@ -1,49 +1,53 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 export default function About() {
     return (
-        <div>
-            <h2>À propos</h2>
-            <div className="row">
-                <div className="col-md-6">
-                    <img
-                        src="https://picsum.photos/480/700?random=24398"
-                        alt=""
-                    />
-                    <figcaption>
-                        Dr Samuel Alain, D.Ps, psychologue
-                        <br />
-                        OPQ: 12907-16
-                    </figcaption>
-                </div>
-                <div className="col-md-6">
-                    <p>
-                        Je suis détenteur d’un baccalauréat en psychologie de
-                        l’Université Laval et d’un doctorat en psychologie
-                        clinique de l’Université du Québec à Chicoutimi. Je suis
-                        psychologue clinicien membre de l’Ordre des psychologues
-                        du Québec. En plus de mon intérêt pour le fonctionnement
-                        humain, j’ai un fort intérêt pour le monde des affaires
-                        que j’actualise par des études de deuxième cycle en
-                        gestion des entreprises. Mon expérience clinique auprès
-                        d’adultes vivant avec diverses problématiques (anxiété,
-                        dépression, difficultés relationnelles, deuil, etc.) m’a
-                        amené à développer une approche professionnelle et
-                        centrée sur les besoins du client. Mes interventions
-                        sont basées sur un savoir scientifique constamment mis à
-                        jour.
-                    </p>
+        <Container fluid={"lg"}>
+            <section>
+                <h2 className={"text-center"}>À propos</h2>
+                <Row>
+                    <Col md={6}>
+                        <img
+                            src="https://picsum.photos/480/400?random=24398"
+                            alt=""
+                        />
+                        <figcaption>
+                            Dr Samuel Alain, D.Ps, psychologue
+                            <br />
+                            OPQ: 12907-16
+                        </figcaption>
+                    </Col>
+                    <Col md={6}>
+                        <p>
+                            Je suis détenteur d’un doctorat en psychologie
+                            clinique de l’Université du Québec à Chicoutimi. Je
+                            suis psychologue clinicien membre de l’Ordre des
+                            psychologues du Québec. Mon expérience clinique
+                            auprès de personnes vivant avec diverses
+                            problématiques (anxiété, dépression, difficultés
+                            relationnelles, deuil, etc.) m’a amené à développer
+                            une approche professionnelle proactive et centrée
+                            sur les besoins du client. Mes interventions sont
+                            basées sur un savoir scientifique constamment mis à
+                            jour.
+                        </p>
 
-                    <p>
-                        Fort d’une expérience clinique variée (réseau de la
-                        santé, réseau de l’éducation, pratique privée), j’adopte
-                        une approche qui s’adapte aux besoins du client. Les
-                        interventions sont donc personnalisées en fonction de la
-                        problématique présentée, des objectifs exprimés et des
-                        caractéristiques personnelles du client.
-                    </p>
-                </div>
-            </div>
-        </div>
+                        <p>
+                            Fort d’une expérience clinique variée (réseau de la
+                            santé, réseau de l’éducation, pratique privée),
+                            j’adopte une approche qui s’adapte à la réalité du
+                            client. Les interventions sont donc personnalisées
+                            en fonction de la problématique présentée, des
+                            objectifs exprimés et des caractéristiques
+                            personnelles du client. Vous bénéficierez d’un
+                            service actif visant une autonomisation rapide face
+                            à la gestion des situations auxquelles vous faites
+                            face.
+                        </p>
+                    </Col>
+                </Row>
+            </section>
+        </Container>
     );
 }
